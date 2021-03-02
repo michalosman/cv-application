a3cffb8c3304845ec7e182ab38e277c7d43a864f
michalosman <michal.osman.997@gmail.com>
2021-03-02 22:27:21 +0100

    Add basic pseudocode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-function App() {
+import React from "react";
+
+const App = () => {
   return <div></div>;
-}
+};
 
 export default App;
+
+// * What is the problem?
+// Write a program that takes user's input and generates a CV which can be downloaded as a PDF file
+
+// * What interface do we need?
+// - Input page
+// - Preview & Download page
+
+// * What functionality do we need?
+// - Get data
+// - Validate data
+// - Store data
+// - Generate CV preview
+// - Generate PDF
+
+// * Which tools may be useful?
+//  styled-components
+//  react-icons
+//  react-to-print
+//  jsToPDF
