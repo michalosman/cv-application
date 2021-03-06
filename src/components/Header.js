dae7ee498b40a5ff2f4cb65c5ee744988b888128
michalosman <michal.osman.997@gmail.com>
2021-03-06 12:37:38 +0100

    Finish header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import { FaFileInvoice } from "react-icons/fa";
 
-const Header = () => {
-  return (
-    <StyledHeader>
-      <Logo>
-        <FaFileInvoice />
-      </Logo>
-      <Title>CV Creator</Title>
-    </StyledHeader>
-  );
-};
-
 const StyledHeader = styled.header`
   display: flex;
   align-items: center;
@@ -20,14 +9,26 @@ const StyledHeader = styled.header`
   background-color: ${({ theme }) => theme.colors.dark};
   color: ${({ theme }) => theme.colors.light};
 `;
+
 const Logo = styled.div`
   display: flex;
-  font-size: 4rem;
   margin-right: 2rem;
+  font-size: 4rem;
 `;
 
-const Title = styled.h1`
+const AppName = styled.h1`
   font-family: "Montserrat", sans-serif;
 `;
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <Logo>
+        <FaFileInvoice />
+      </Logo>
+      <AppName>CV Creator</AppName>
+    </StyledHeader>
+  );
+};
 
 export default Header;
