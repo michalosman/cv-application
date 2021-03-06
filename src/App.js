6a4aa95358a2d11ae74b78248b2027f997d001fd
michalosman <michal.osman.997@gmail.com>
2021-03-06 10:04:33 +0100

    Add basic styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
